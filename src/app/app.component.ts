6a38bb74094d27d65709636a498a5e083f3b734d
ulflunde <ulf.lunde@gmail.com>
2017-02-24 10:22:38 +0100

    Enabled editing of our hero.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,11 @@ export class Hero {
   <h1>{{title}}</h1>
   <h2>{{hero.name}} details!</h2>
   <div><label>id: </label>{{hero.id}}</div>
-  <div><label>name: </label>{{hero.name}}</div>
+  <div>
+    <label>name: </label>
+    <input value="{{hero.name}}" placeholder="name">
+    <input [(ngModel)]="hero.name" placeholder="name">
+  </div>
   `
 })
 
