c6c536d302155a50313ca7901c468c42909c49dd
ulflunde <ulf.lunde@gmail.com>
2017-02-24 10:10:45 +0100

    Show our hero.

--- src/app/app.component.js
+++ src/app/app.component.js
@@ -11,12 +11,13 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 var core_1 = require('@angular/core');
 var AppComponent = (function () {
     function AppComponent() {
-        this.name = 'Angular';
+        this.title = 'Tour of Heroes';
+        this.hero = 'Windstorm';
     }
     AppComponent = __decorate([
         core_1.Component({
             selector: 'my-app',
-            template: "<h1>Hello {{name}}</h1>",
+            template: '<h1>{{title}}</h1><h2>{{hero}} details!</h2>'
         }), 
         __metadata('design:paramtypes', [])
     ], AppComponent);
